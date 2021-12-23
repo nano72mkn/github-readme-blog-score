5a9ec885aa0b35d410a3c3a42b7162363957e2df
Shota Miyahira <2525.natuta@gmail.com>
2021-12-24 01:22:19 +0900

    数字と名前だけ

--- pages/api/get_zenn_score.ts
+++ pages/api/get_zenn_score.ts
@@ -2,12 +2,47 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { SvgBase } from '../../src/common/svgBase';
 
-const getZennScore = (
+type QueryType = {
+  zennId?: string;
+}
+
+type ZennUserData = {
+  username: string;
+  avatar_small_url: string;
+  avatar_url: string;
+  total_liked_count: number;
+  follower_count: number;
+  following_count: number;
+  articles_count: number;
+  books_count: number;
+  scraps_count: number;
+}
+
+const getZennScore = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
+  const { zennId }: QueryType = req.query
+
+  if (!zennId) {
+    res.status(404).end();
+  };
+
+  const data = await fetch(`https://zenn.dev/api/users/${zennId}`);
+  const { status } = data;
+  
+  if (status !== 200) {
+    res.status(status).end();
+  }
+
+  const { user }: { user: ZennUserData } = await data.json();
+
   const svgBase = new SvgBase();
-  const svg = svgBase.render(`<circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />`);
+  const svg = svgBase.render(`
+    <text x="0" y="10" font-family="Verdana" font-size="10">${user.username}</text>
+    <text x="0" y="25" font-family="Verdana" font-size="10">${user.articles_count}</text>
+    <text x="0" y="35" font-family="Verdana" font-size="10">${user.total_liked_count}</text>
+  `);
 
   res.statusCode = 200;
   res.setHeader("Content-Type", "image/svg+xml");
